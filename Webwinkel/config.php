13e6fb32bb1590491cf461a65be019b8b82c8d84
80508 <46776012+80508@users.noreply.github.com>
2019-07-03 12:19:51 +0200

    Delete config.php

--- Webwinkel/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// database logingegevens
-$db_hostname = 'localhost';
-$db_username = '80508';
-$db_password = 'zk0fi908';
-$db_database = '80508_DB';
-
-// maak de database-verbinding
-$mysqli = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
